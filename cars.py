74771ef363e303319a961f4ead4efa9d3ee32cee
aziza <60103065@udst.edu.qa>
2024-01-31 11:09:51 +0300

    Second update of cars.py

--- cars.py
+++ cars.py
@@ -1,6 +1,12 @@
 import random
 
+def getModelYeal(years):
+    return random.choice(years)
+
 print("Hello World!")
 
 cars = ["Toyota", "BMW", "Ford", "Cadillac", "Audi", "Tesla", "Porsche"]
+modelYear = ["2010", "2012", "2014"]
+
 print(cars[random.randint(0, len(cars)-1)])
+print(getModelYeal(modelYear))
